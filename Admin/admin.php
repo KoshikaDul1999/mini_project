237830ba2169d9b4e7a180bff743534ab129570d
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2022-05-02 13:13:55 +0530

    update admin.php

--- Admin/admin.php
+++ Admin/admin.php
@@ -4,23 +4,35 @@
 
 <?php
 
-if(isset($_POST['username'])){
+if(isset($_POST['login'])){
 
     $uname=$_POST['username'];
     $password=$_POST['password'];
 
-    $sql="select * from adminlogin where admin_user_name='".$uname."'AND admin_password='".$password."' limit 1";
+    $sql="SELECT admin_user_name,admin_password FROM adminlogin WHERE admin_user_name='$uname' AND admin_password='$password'";
 
     $result=mysqli_query($con,$sql);
 
-    if(mysqli_num_rows($result)==1){
-        //echo "You have successfully logged in";
-        exit();
+    if(!$result)
+    {
+        die('Could not get data: ' . mysqli_error($con));
     }
-    else{
-        echo "You have entered Incorret Password";
-        exit();
+
+    if(mysqli_num_rows($result)<1){
+        echo "<center> Please enter a valid user name!";
     }
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        if($password != $row['admin_password'])
+        {
+            echo "Password is incorrect";
+        }else{
+            header('Location:admindashboard.php');
+        }
+    }
+    
+    
 }
 
 ?>
@@ -32,14 +44,14 @@ if(isset($_POST['username'])){
     <body>
         <div class="container">
             <img src="images/admin.jpg">
-            <form method="POST" action="admindashboard.php">
+            <form method="POST">
                 <div class="form-input">
                     <input type="text" name="username" placeholder="User Name">
                 </div>
                 <div class="form-input">
                     <input type="password" name="password" placeholder="Password">
                 </div>
-                <input type="submit" value="LOGIN" class="btn-login">
+                <input type="submit" name="login" value="LOGIN" class="btn-login">
             </form>
         </div>
     </body>
